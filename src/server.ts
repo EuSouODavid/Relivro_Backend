7f3481b14b44dc7e70c62dda8b0f399d65102489
Louise <vahallouise@gmail.com>
2026-09-11 09:58:12 -0300

    Implementar CRUD de clientes e login

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 
-// import routesMarcas from './routes/marcas'
 import routesLivros from './routes/livros'
-// import routesClientes from './routes/clientes'
-// import routesLogin from './routes/login'
-// import routesPropostas from './routes/propostas'
+import routesClientes from './routes/clientes'   
+import routesLogin from './routes/login'
 
 const app = express()
 const port = 3000
@@ -13,11 +11,9 @@ const port = 3000
 app.use(express.json())
 app.use(cors())
 
-// app.use("/marcas", routesMarcas)
 app.use("/livros", routesLivros)
-// app.use("/clientes", routesClientes)
-// app.use("/clientes/login", routesLogin)
-// app.use("/propostas", routesPropostas)
+app.use("/clientes", routesClientes)  
+app.use("/clientes/login", routesLogin) 
 
 app.get('/', (req, res) => {
   res.send('API: ReLivro')
